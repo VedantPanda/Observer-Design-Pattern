c40365a18668bd1e410c2a11e072cbeded98667a
vedantpanda <ved07panda@gmail.com>
2024-08-01 21:55:05 +0530

    Observer design pattern

--- src/main/WeatherStation.java
+++ src/main/WeatherStation.java
@@ -9,5 +9,6 @@ public class WeatherStation {
         weatherData.setMeasurements(19,20,21);
         weatherData.setMeasurements(22,23,24);
         weatherData.setMeasurements(25,26,27);
+
     }
 }
