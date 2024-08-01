d33a1c809c49c3b2a9c6be8de6292c13c6e2491c
vedantpanda <ved07panda@gmail.com>
2024-08-01 22:03:37 +0530

    Observer design pattern

--- src/main/WeatherStation.java
+++ src/main/WeatherStation.java
@@ -2,6 +2,7 @@ package main;
 
 public class WeatherStation {
     public static void main(String[] args) {
+
         WeatherData weatherData = new WeatherData();
         new CurrentConditionsDisplay(weatherData);
         new StatisticsDisplay(weatherData);
@@ -9,5 +10,6 @@ public class WeatherStation {
         weatherData.setMeasurements(19,20,21);
         weatherData.setMeasurements(22,23,24);
         weatherData.setMeasurements(25,26,27);
+
     }
 }
